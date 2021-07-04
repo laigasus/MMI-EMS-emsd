194ce76893a81ed01efeb0daa2702bd2ec4e79f5
Ok Jae Ook <okjaeook98@gmail.com>
2021-07-04 21:14:49 +0900

    코드 간결화

--- agtd.c
+++ agtd.c
@@ -1,167 +1,148 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/msg.h>
 #include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // 192.168.1.128, 192.168.1.145
 
-int mmi_socket_open(void) {
-	int mmi_server_socket;
-	struct sockaddr_in serv_addr = {
-		.sin_family = AF_INET,
-		.sin_addr.s_addr = htonl(INADDR_ANY),
-		.sin_port = htons(9000)
-	};
-	mmi_server_socket = socket(PF_INET, SOCK_STREAM, 0);
-	if (mmi_server_socket < 0) return -1;
+typedef struct msgq_data {
+  long type;
+  char text[2048];
+} Message;
 
-	if (bind(mmi_server_socket, (struct sockaddr*)&serv_addr, sizeof serv_addr) < 0) return -2;
+int socket_open(char* target) {
+  int sock, target_port;
 
-	if (listen(mmi_server_socket, 5) < 0) return -3;
+  if (!strcmp(target, "mmi")) {
+    target_port = 9000;
+  }
+  if (!strcmp(target, "stdby")) {
+    target_port = 9001;
+  }
+  struct sockaddr_in serv_addr = {.sin_family = AF_INET,
+                                  .sin_addr.s_addr = htonl(INADDR_ANY),
+                                  .sin_port = htons(target_port)};
 
-	return mmi_server_socket;
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+  if (sock < 0) return -1;
+
+  if (bind(sock, (struct sockaddr*)&serv_addr, sizeof serv_addr) < 0) return -2;
+
+  if (listen(sock, 5) < 0) return -3;
+
+  return sock;
 }
 
-int stdby_socket_open(void) {
-	int stdby_server_socket;
-	struct sockaddr_in serv_addr = {
-		.sin_family = AF_INET,
-		.sin_addr.s_addr = htonl(INADDR_ANY),
-		.sin_port = htons(9001)
-	};
-	stdby_server_socket = socket(PF_INET, SOCK_STREAM, 0);
-	if (stdby_server_socket < 0) return -1;
+int mmi_server_worker(int clnt_sock, char* buf) {
+  int i = 0;
+  char* arg[3] = {"", "", ""};
+  char send_buf[2048] = "";
 
-	if (bind(stdby_server_socket, (struct sockaddr*)&serv_addr, sizeof serv_addr) < 0) return -2;
+  int qid, len;
 
-	if (listen(stdby_server_socket, 5) < 0) return -3;
+  // 아규먼트 토큰 분리
+  for (char* p = strtok(buf, "\n"); p; p = strtok(NULL, "\n")) {
+    arg[i++] = p;
+  }
+  printf("토큰 분리 성공\n");
 
-	return stdby_server_socket;
+  Message send_data = {1L, *arg[1]};
+  Message recv_data;
+
+  printf("%s\n", arg[0]);
+  printf("%s\n", arg[1]);
+  if ((qid = msgget((key_t)0111, IPC_CREAT | 0666)) == -1) {
+    perror("메시지 큐 생성 실패\n");
+  }
+
+  //아규먼트 별 명령 실행
+  if (strcmp(arg[0], "DIS-RESOURCE") == 0) {
+    if (strcmp(arg[1], "MEMORY") == 0) {
+      printf("memory 실행\n");
+      send_data.type = 1;
+      sprintf(send_data.text, "%s", arg[1]);
+      printf("send 실행\n");
+      if (msgsnd(qid, &send_data, strlen(send_data.text), 0) == -1) {
+        perror("메시지 큐 전송 실패\n");
+      }
+      if ((len = msgrcv(qid, &recv_data, 100, 0, 0)) == -1) {
+        perror("메시지 큐 수신 실패\n");
+      }
+      sprintf(send_buf, "%s", recv_data.text);
+      if (msgctl(qid, IPC_RMID, 0) == -1) {
+        perror("msgctl 실패\n");
+      }
+    } else if (strcmp(arg[1], "DISK")) {
+      if (msgsnd(qid, &send_data, strlen(send_data.text), 0) == -1) {
+        perror("메시지 큐 전송 실패\n");
+      }
+      if ((len = msgrcv(qid, &recv_data, 100, 0, 0)) == -1) {
+        perror("메시지 큐 수신 실패\n");
+      }
+      sprintf(send_buf, "%s", recv_data.text);
+      if (msgctl(qid, IPC_RMID, 0) == -1) {
+        perror("msgctl 실패\n");
+      }
+    } else if (strcmp(arg[1], "CPU")) {
+      if (msgsnd(qid, &send_data, strlen(send_data.text), 0) == -1) {
+        perror("메시지 큐 전송 실패\n");
+      }
+      if ((len = msgrcv(qid, &recv_data, 100, 0, 0)) == -1) {
+        perror("메시지 큐 수신 실패\n");
+      }
+      sprintf(send_buf, "%s", recv_data.text);
+      if (msgctl(qid, IPC_RMID, 0) == -1) {
+        perror("msgctl 실패\n");
+      }
+    } else {
+      char msg[30] = "명령어 잘못 입력\n";
+      sprintf(send_buf, "%s", msg);
+    }
+
+  } else if (strcmp(arg[0], "DIS-SW-STS")) {
+  }
+  write(clnt_sock, send_buf, strlen(send_buf));
+  close(clnt_sock);
 }
 
+void msg_queue() {}
 
-int mmi_server_worker(int clnt_sock, char *buf) {
-	int i = 0;
-	char arg[4] = { "", "", "" };
-	char send_buf[2048] = "";
+void main() {
+  // 서버 오픈, accept에 사용 할 변수 선언, read에 사용 할 변수 선언
+  int mmi_client_socket;
+  struct sockaddr_in clnt_addr;
+  int clnt_addr_size;
+  int mmi_server_socket = socket_open("mmi"), recv_len;  // 서버 오픈 함수
+  char buf[2048];
 
-	int qid,len;
-	
-	// 아규먼트 토큰 분리
-	for (char* p = strtok(buf, "\n"); p; p = strtok(NULL, "\n")) {
-		arg[i++] = p;
-	}
-	printf("토큰 분리 성공\n");
+  switch (mmi_server_socket) {
+    case -1:
+      perror("소켓 생성 실패\n");
+      exit(-1);
+    case -2:
+      perror("바인드 실패\n");
+      exit(-2);
+    case -3:
+      perror("listen 실패\n");
+      exit(-3);
+  }
 
-	struct msgq_data {
-		long type;
-		char text[2048];
-	};
-	struct msgq_data send_data;
-	struct msgq_data recv_data;
+  while (1) {
+    clnt_addr_size = sizeof(clnt_addr);
+    mmi_client_socket = accept(mmi_server_socket, (struct sockaddr*)&clnt_addr,
+                               &clnt_addr_size);
+    printf("mmi 연결 성공\n");
+    recv_len = read(mmi_client_socket, buf, sizeof buf);
+    if (recv_len < 0) continue;
+    buf[recv_len] = '\0';
+    printf("%s\n", buf);
 
-
-	printf("%s\n",arg[0]);
-	printf("%s\n", arg[1]);
-	if ((qid = msgget((key_t)0111, IPC_CREAT | 0666)) == -1) {
-		printf("메시지 큐 생성 실패\n");
-	}
-
-	//아규먼트 별 명령 실행
-	if (strcmp(arg[0], "DIS-RESOURCE") == 0) {
-		if (strcmp(arg[1], "MEMORY") == 0) {
-			printf("memory 실행\n");
-			send_data.type = 1;
-			sprintf(send_data.text, "%s", arg[1]);
-			printf("send 실행\n");
-			if (msgsnd(qid, &send_data, strlen(send_data.text), 0) == -1) {
-				printf("메시지 큐 전송 실패\n");
-			}
-			if ((len = msgrcv(qid, &recv_data, 100, 0, 0)) == -1)
-			{
-				printf("메시지 큐 수신 실패\n");
-			}
-			sprintf(send_buf, "%s", recv_data.text);
-			if (msgctl(qid, IPC_RMID, 0) == -1) {
-				printf("msgctl 실패\n");
-			}
-		}
-		else if (strcmp(arg[1], "DISK")) {
-			struct msgq_data send_data = { 1L, arg[1] };
-			struct msgq_data recv_data;
-			if (msgsnd(qid, &send_data, strlen(send_data.text), 0) == -1) {
-				printf("메시지 큐 전송 실패\n");
-			}
-			if ((len = msgrcv(qid, &recv_data, 100, 0, 0)) == -1)
-			{
-				printf("메시지 큐 수신 실패\n");
-			}
-			sprintf(send_buf, "%s", recv_data.text);
-			if (msgctl(qid, IPC_RMID, 0) == -1) {
-				printf("msgctl 실패\n");
-			}
-		}
-		else if (strcmp(arg[1], "CPU")) {
-			struct msgq_data send_data = { 1L, arg[1] };
-			struct msgq_data recv_data;
-			if (msgsnd(qid, &send_data, strlen(send_data.text), 0) == -1) {
-				printf("메시지 큐 전송 실패\n");
-			}
-			if ((len = msgrcv(qid, &recv_data, 100, 0, 0)) == -1)
-			{
-				printf("메시지 큐 수신 실패\n");
-			}
-			sprintf(send_buf, "%s", recv_data.text);
-			if (msgctl(qid, IPC_RMID, 0) == -1) {
-				printf("msgctl 실패\n");
-			}
-		}
-		else {
-			char msg[20] = "명령어 잘못 입력\n";
-			sprintf(send_buf, "%s", msg);
-		}
-
-	}
-	else if (strcmp(arg[0], "DIS-SW-STS")) {
-
-	}
-	write(clnt_sock, send_buf, strlen(send_buf));
-	close(clnt_sock);
+    mmi_server_worker(mmi_client_socket, buf);  //받은 명령어 실행 코드
+  }
 }
-
-int main(void) {
-	// 서버 오픈, accept에 사용 할 변수 선언, read에 사용 할 변수 선언
-	int mmi_client_socket;
-	struct sockaddr_in clnt_addr;
-	int clnt_addr_size;
-	int mmi_server_socket = mmi_socket_open(), recv_len; // 서버 오픈 함수
-	char buf[2048];
-
-	switch (mmi_server_socket)
-	{
-	case -1: printf("소켓 생성 실패\n"); return 1;
-	case -2: printf("바인드 실패\n"); return 1;
-	case -3: printf("listen 실패\n"); return 1;
-	}
-
-	while (1) {
-		clnt_addr_size = sizeof(clnt_addr);
-		mmi_client_socket = accept(mmi_server_socket, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-		printf("mmi 연결 성공\n");
-		recv_len = read(mmi_client_socket, buf, sizeof buf);
-		if (recv_len < 0) continue;
-		buf[recv_len] = '\0';
-		printf(buf);
-
-		mmi_server_worker(mmi_client_socket, buf); //받은 명령어 실행 코드
-
-
-	}
-}
-
--- rstat.c
+++ rstat.c
@@ -1,66 +1,60 @@
-#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <sys/types.h>
 
 #define BUFSIZE 40
-#define QKEY    (key_t)0111
+#define QKEY (key_t)0111
 
-struct msgq_data {
-    long type;
-    char text[BUFSIZE];
-};
-struct msgq_data recv_data;
-struct msgq_data send_data;
+typedef struct msgq_data {
+  long type;
+  char text[BUFSIZE];
+} Message;
 
-main()
-{
-   int qid, len;
-  
-    if ((qid = msgget(QKEY, IPC_CREAT | 0666)) == -1) {
-        perror("msgget failed");
-        exit(1);
+void main() {
+  int qid, len;
+  char tmp[2048];
+  char fcnt[BUFSIZE] = "";
+
+  Message recv_data, send_data;
+
+  if ((qid = msgget(QKEY, IPC_CREAT | 0666)) == -1) {
+    perror("msgget failed");
+    exit(1);
+  }
+  if ((len = msgrcv(qid, &recv_data, BUFSIZE, 0, 0)) == -1) {
+    perror("msgrcv failed");
+    exit(1);
+  }
+  if (msgctl(qid, IPC_RMID, 0) == -1) {
+    perror("msgctl failed");
+    exit(1);
+  }
+
+  printf("%s\n", recv_data.text);
+
+  if (recv_data.text == "CPU") {
+    FILE* fp = popen("top -n 1 -b | awk '/^%Cpu/{print $2}'", "r");
+
+    fgets(fcnt, sizeof fcnt, fp);
+    printf("%s", fcnt);
+
+  } else if (strcmp(recv_data.text, "MEMORY") == 0) {
+    FILE* fp = popen("free | grep Mem | awk '{print $4/$3 * 100.0}'", "r");
+    fgets(fcnt, sizeof fcnt, fp);
+    printf("%s", fcnt);
+    send_data.type = 1;
+    sprintf(send_data.text, "%s", fcnt);
+    msgsnd(qid, &send_data, strlen(send_data.text), 0);
+    if (msgctl(qid, IPC_RMID, 0) == -1) {
+      perror("msgctl failed");
+      exit(1);
     }
-    if ((len = msgrcv(qid, &recv_data, BUFSIZE, 0, 0)) == -1) {
-        perror("msgrcv failed");
-        exit(1);
-    }
-    //if (msgctl(qid, IPC_RMID, 0) == -1) {
-        //perror("msgctl failed");
-       // exit(1);
-    //}
-    printf("%s\n", recv_data.text);
- 
-    if (recv_data.text == "CPU") {
-        char tmp[2048];
-        FILE* fp = popen("top -n 1 -b | awk '/^%Cpu/{print $2}'", "r");
-        char fcnt[32] = "";
-        fgets(fcnt, sizeof fcnt, fp);
-        printf("%s", fcnt);
-        
-    }
-    else if (strcmp(recv_data.text,"MEMORY") == 0) {
-        char tmp[2048];
-        FILE* fp = popen("free | grep Mem | awk '{print $4/$3 * 100.0}'", "r");
-        char fcnt[40] = "";
-        fgets(fcnt, sizeof fcnt, fp);
-        printf("%s", fcnt);
-        send_data.type = 1;
-        sprintf(send_data.text, "%s", fcnt);
-        msgsnd(qid, &send_data, strlen(send_data.text), 0);
-        if (msgctl(qid, IPC_RMID, 0) == -1) {
-            perror("msgctl failed");
-            exit(1);
-        }
-    }
-    else if (recv_data.text == "DISK") {
-        char tmp[2048];
-        FILE* fp = popen("df|tail -1|tr -s ' '|cut -d ' ' -f5", "r");
-        char fcnt[32] = "";
-        fgets(fcnt, sizeof fcnt, fp);
-        printf("%s", fcnt);
-    }
- 
+  } else if (recv_data.text == "DISK") {
+    FILE* fp = popen("df|tail -1|tr -s ' '|cut -d ' ' -f5", "r");
+    fgets(fcnt, sizeof fcnt, fp);
+    printf("%s", fcnt);
+  }
 }
